c4e2b3439700590cdc417ccfe340d6d426ef57cf
AnthonyR <rodrianthonys2004@gmail.com>
2024-11-15 21:42:01 -0500

    Subiendo metodo para guardar solicitud del cliente desde la web

---
+++ MotorsApi/BD/CRUD/Create/Solicitudes.cs
@@ -0,0 +1,58 @@
+﻿using MotorsApi.Models;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace MotorsApi.BD.CRUD.Create
+{
+    public class Solicitudes : Conexiondb
+    {
+
+        //Metodo para cargar tabla Solicitud
+        public int registrarSolicitud(Solicitud solicito)
+        {
+
+            //declaracion de variables de trabajo
+            int insercion;
+
+            try
+            {
+                //Limpiamos parametros
+                cmd.Parameters.Clear();
+
+                //asignamos el tipo codigo 
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //asignamos el nombre del procedimiento de almacendo
+                cmd.CommandText = "sp_solicitudAuto"; //poner nombre posteriormente
+
+                //asignamos parametros
+                cmd.Parameters.Add(new MySqlParameter("@id_usuario",solicito.id_usuario));
+                cmd.Parameters.Add(new MySqlParameter("@descripcion",solicito.descripcion));
+                cmd.Parameters.Add(new MySqlParameter("@estado",solicito.estado));
+                cmd.Parameters.Add(new MySqlParameter("@f_solicitud",solicito.f_solicitud));
+                cmd.Parameters.Add(new MySqlParameter("@foto",solicito.foto));
+                cmd.Parameters.Add(new MySqlParameter("@monto",solicito.monto));
+
+                //abrir Conexion
+                abrirConexion();
+
+                //validamos si se inserto el auto
+                insercion = Convert.ToInt32(cmd.ExecuteNonQuery());
+
+                if (insercion > 0)
+                {
+                    return insercion;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("No se pudo guardar la solicitud"+e);
+            }
+            finally
+            {
+                cerrarConexion();
+            }
+            return 0;
+        }
+    }
+}
--- MotorsApi/Models/Solicitud.cs
+++ MotorsApi/Models/Solicitud.cs
@@ -7,7 +7,6 @@ namespace MotorsApi.Models
         //Atributos de la entidad
         public int id_solicitud { get; set; }
         public int id_usuario { get; set; }
-        public string id_vehiculo { get; set; }
         public string descripcion { get; set; }
         public string estado { get; set; }
         public DateTime f_solicitud { get; set; }
