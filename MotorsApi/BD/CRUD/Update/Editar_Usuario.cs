475df4b2b7d36aca3a608d71cad2a81805ce592f
Naturase <asaenz299@gmail.com>
2024-11-18 20:08:07 -0500

    Aquí se eliminó el método extra, me equivoqué, sorri

--- MotorsApi/BD/CRUD/Update/Editar_Usuario.cs
+++ MotorsApi/BD/CRUD/Update/Editar_Usuario.cs
@@ -6,51 +6,6 @@ namespace MotorsApi.BD.CRUD.Update
 {
     public class Editar_Usuario : Conexiondb
     {
-        //Editasmos Telefono de Usuario
-        //public int UsuarioEditar ( Usuario usuario, Login login)
-        //{
-        //    try
-        //    {
-        //        //Limpiamos parametros
-        //        cmd.Parameters.Clear();
-
-        //        //asignamos el tipo
-        //        cmd.CommandType = CommandType.Text;
-
-        //        //asignamos el cod
-        //        cmd.CommandText = "UPDATE Usuario SET telefono = @telefono  WHERE id = @id";
-
-        //        //asignamos parametros
-        //        cmd.Parameters.Add(new MySqlParameter("@id", usuario.id));
-        //        cmd.Parameters.Add(new MySqlParameter("@telefono", usuario.telefono));
-        //        cmd.Parameters.Add(new MySqlParameter("@f_actualizacion", usuario.f_actualizacion));
-
-
-        //        //Abrimos conexion
-        //        abrirConexion();
-
-        //        //Validacion de Actualizacion
-        //        int insertedId = Convert.ToInt32(cmd.ExecuteNonQuery());
-        //        if (insertedId > 0)
-        //        {
-        //            return insertedId;
-        //        }
-
-
-        //    }
-
-        //    catch (Exception e)
-        //    {
-
-        //        Console.WriteLine("No pudo actualizarse" + e);
-        //    }
-        //    finally
-        //    {
-        //        //Cerramos conexion
-        //        cerrarConexion();
-        //    }
-        //    return 0;
-        //}
 
         public int EditarUsuario(int id, ActualizarUsuario usuario)
         {
