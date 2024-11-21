8518c92e285cc1b383b8c21db4f2f8f845237bb1
Naturase <asaenz299@gmail.com>
2024-11-20 20:50:07 -0500

    API ya puede guardar carros vendidos y cambiar la disponibilidad de carros vendidos

--- MotorsApi/Controllers/ventaFlotaController.cs
+++ MotorsApi/Controllers/ventaFlotaController.cs
@@ -15,7 +15,7 @@ namespace MotorsApi.Controllers
 
         [HttpPost]
         [Route("venta")] 
-        public object VenderAuto(Flota_Venta venta)
+        public object VenderAuto(Flota_Venta venta) //Este método agrega a la tabla Flota_Venta, el carro pasa a venderse
         {
             Venta_Flota ventaAuto = new Venta_Flota();
 
@@ -38,8 +38,45 @@ namespace MotorsApi.Controllers
 
         }
 
-    
+        [HttpPost]
+        [Route ("vendido/{id}")]
+        public IActionResult CarroVendido(int id, Ventas vendido)
+        {
+            if (vendido == null || id == null)
+            {
+                return BadRequest(new
+                {
+                    titulo = "Datos inválidos",
+                    Mensaje = "Datos o id nulos",
+                    Code = 400
+                });
+            }
 
+            Venta_Flota agregar = new Venta_Flota();
+            bool dispo = false;
+
+            var guardado = agregar.VenderCarro(id, vendido); //Para saber si se ingresaron los datos
+            var cambiado = agregar.CambiarDisponibilidad(vendido.id_vehiculo, dispo); //para saber si se cambió la disponibilidad
+
+            if (guardado > 0 || cambiado > 0)
+            {
+                return Ok(new
+                {
+                    titulo = "Éxito al guardar",
+                    Mensaje = "Los datos se han guardado correctamente.",
+                    Code = 200
+                });
+            }
+            else
+            {
+                return StatusCode(500, new
+                {
+                    titulo = "Error al guardar",
+                    Mensaje = "El carro no se pudo vender.",
+                    Code = 500
+                });
+            }
+        }
 
     }
 }
