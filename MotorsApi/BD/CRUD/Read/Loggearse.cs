f4d477408167ec4ab290f7d0181f588241ceacce
AnthonyR <rodrianthonys2004@gmail.com>
2024-11-21 18:52:24 -0500

    Funcionalidad para obtener el nombre,apellido,telefono y correo del cliente

--- MotorsApi/BD/CRUD/Read/Loggearse.cs
+++ MotorsApi/BD/CRUD/Read/Loggearse.cs
@@ -88,5 +88,53 @@ namespace MotorsApi.BD.CRUD.Read
 
         }
 
+        //metodo para obtener informacion del usuario {nombre, correo y telefono}
+        public List<UsuarioRequest> obtenerUsuario(int id) {
+        
+            List<UsuarioRequest> cliente = new List<UsuarioRequest>();
+            UsuarioRequest usuario = new UsuarioRequest();
+            try
+            {
+                // Limpiamos los parámetros
+                cmd.Parameters.Clear();
+
+                // Asignamos el tipo de comando
+                cmd.CommandType = CommandType.Text;
+
+                // Consulta SQL con parámetros para evitar inyecciones SQL
+                cmd.CommandText = "SELECT Usuario.nombre, Usuario.apellido, Usuario.telefono, Login.correo FROM  Usuario JOIN Login ON Usuario.id = Login.id_usuario WHERE Usuario.id = @id";
+
+                // Agregamos los parámetros
+                cmd.Parameters.AddWithValue("@id", id);
+  
+
+                // Abrimos la conexión
+                abrirConexion();
+
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read()) // Verifica si hay registros
+                    {
+                        usuario.nombre = reader.GetString(0);
+                        usuario.apellido = reader.GetString(1);
+                        usuario.telefono = reader.GetString(2);
+                        usuario.correo = reader.GetString(3);
+                    }
+
+                    cliente.Add(usuario);
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            finally {
+
+                cerrarConexion();
+            }
+            
+            return cliente;
+        }
+
     }
 }
---
+++ MotorsApi/Models/UsuarioRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace MotorsApi.Models
+{
+    public class UsuarioRequest
+    {
+        public string nombre {  get; set; }
+        public string apellido { get; set; }
+        public string telefono { get; set; }
+        public string correo { get; set; }
+
+    }
+}
