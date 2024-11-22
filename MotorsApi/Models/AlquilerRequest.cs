f653ef968714c767d88e3d8c3e973401728a1180
patrickvillarroel <130080309+patrickvillarroel@users.noreply.github.com>
2024-11-21 19:19:36 -0500

    Metodos para el Create de Alquiler

---
+++ MotorsApi/Models/AlquilerRequest.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotorsApi.Models
+{
+    public class AlquilerRequest
+    {
+        [Required]
+        public string placa { get; set; } = string.Empty; // Obligatorio
+
+        [Required]
+        public int idUsuario { get; set; } // Obligatorio
+
+        [Required]
+        public int tipoTarifa { get; set; } // Obligatorio
+
+        [Required]
+        public DateTime fechaRetiro { get; set; } // Obligatorio
+
+        [Required]
+        public DateTime fechaEntrega { get; set; } // Obligatorio
+
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "El precio total debe ser mayor o igual a 0.")]
+        public double precioTotal { get; set; } // Obligatorio y debe ser un número válido
+
+        [Required]
+        public int idSeguro { get; set; } // Obligatorio
+    }
+}
