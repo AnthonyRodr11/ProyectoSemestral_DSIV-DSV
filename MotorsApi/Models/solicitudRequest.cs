dec8e67020f31dff448d37020b64600d4c9ce327
AnthonyR <rodrianthonys2004@gmail.com>
2024-11-24 13:46:06 -0500

    arreglando clase solicitudRequest

--- MotorsApi/Models/solicitudRequest.cs
+++ MotorsApi/Models/solicitudRequest.cs
@@ -1,16 +1,14 @@
 ﻿namespace MotorsApi.Models
 {
-    namespace MotorsApi.Models
+    public class solicitudRequest
     {
         //Atributos 
- 
+
         public int id_usuario { get; set; }
         public string descripcion { get; set; }
         public string estado { get; set; }
         public double monto { get; set; }
 
 
-        
     }
 }
-
