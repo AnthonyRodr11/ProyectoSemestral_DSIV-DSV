64364fb1f86390f244d10eff9f6d1ab92d08b34b
LelouuAK <151658285+LelouuAK@users.noreply.github.com>
2024-11-25 18:27:50 -0500

    No face

--- MotorsApi/BD/CRUD/Read/Ver_Flotas.cs
+++ MotorsApi/BD/CRUD/Read/Ver_Flotas.cs
@@ -519,63 +519,7 @@ namespace MotorsApi.BD.CRUD.Read
 
 
 
-        //Metodo para obtener la lista de autos en subasta
-        public List<Flota_AlquilerRequest> listaAlquiler(int id_usuario)
-        {
-            //Lista de objetos
-            List<Flota_AlquilerRequest> lista = new List<Flota_AlquilerRequest>();
-
-            try
-            {
-                //Limpiamos parametros
-                cmd.Parameters.Clear();
-
-                //Especificamos el tipo de comando
-                cmd.CommandType = CommandType.Text;
-
-                //asignamos consulta a realizar
-                cmd.CommandText = "SELECT cod_alquiler, id_vehiculo, f_retiro, f_entregas FROM Flota_Alquiler WHERE  id_usuario = @id_usuario";
-
-                cmd.Parameters.AddWithValue("@id_usuario", id_usuario);
-
-                abrirConexion();
-
-                using (MySqlDataReader reader = cmd.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        Flota_AlquilerRequest facturita = new Flota_AlquilerRequest()
-                        {
-                            id_vehiculo = reader["marca"].ToString(),
-                            cod_alquiler = Convert.ToInt32(reader["cod_alquiler"]),
-                            f_entrega = Convert.ToDateTime(reader["f_entrega"]),
-                            f_retiro = Convert.ToDateTime(reader["f_retiro"])
-
-                        };
-
-                        lista.Add(facturita);
-                    };
-
-                }
-
-
-            }
-            catch (Exception e)
-            {
-                throw;
-            }
-            finally
-            {
-
-                cerrarConexion();
-
-            }
-            return lista;
-
-
-
-
-        }
+        
 
 
     }
