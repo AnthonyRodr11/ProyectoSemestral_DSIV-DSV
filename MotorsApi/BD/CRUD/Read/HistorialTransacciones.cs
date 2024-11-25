16dafc6f76250a45270bcd6f7b1b9c6f25b51a8f
AnthonyR <rodrianthonys2004@gmail.com>
2024-11-25 18:14:25 -0500

    Subiendo clase que hara las lecturas de transacciones

---
+++ MotorsApi/BD/CRUD/Read/HistorialTransacciones.cs
@@ -0,0 +1,20 @@
+﻿using MotorsApi.Models;
+
+namespace MotorsApi.BD.CRUD.Read
+{
+    public class HistorialTransacciones
+    {
+        //Esta clase contendra los historial de transacciones para el usuario mandando el id_usuario
+
+        //Transacciones de Alquileres
+        public List<Flota_AlquilerRequest> obtenerMisAlquileres(int id_usario)
+        {
+
+        }
+        //Transacciones de Venta
+
+
+        //Transacciones de Subasta
+
+    }
+}
