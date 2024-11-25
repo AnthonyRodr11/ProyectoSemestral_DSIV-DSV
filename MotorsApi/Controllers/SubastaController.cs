b813396b2a589422a02b9d5f8fe712ce4659ba76
Naturase <asaenz299@gmail.com>
2024-11-25 18:25:50 -0500

    Agregando controller para Subasta

---
+++ MotorsApi/Controllers/SubastaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MotorsApi.BD.CRUD.Read;
+
+namespace MotorsApi.Controllers
+{
+    public class SubastaController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("subasta/{placa}")]
+        public IActionResult GetSubasta(string placa)
+        {
+            var tsubaru = new Ver_Flotas().listaSubasta(placa);
+
+            if(tsubaru == null)
+            {
+                return NotFound("Error al cargar subasta");
+            }
+            return Ok(tsubaru);
+        }
+
+
+    }
+}
